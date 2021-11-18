df88f8c6ff165ee2baac76e60ae6cbb6757cb432
EmreKabali <50817604+EmreKabali@users.noreply.github.com>
2021-11-18 23:24:17 +0300

    added   new models

---
+++ Entity/Models/SubMenuItems.cs
@@ -0,0 +1,23 @@
+﻿using Entity.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Models
+{
+    public class SubMenuItems:ModelBase
+    {
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Path { get; set; }
+
+        public string Paremeters { get; set; }
+
+        public int ParentMenuId { get; set; }
+    }
+}
