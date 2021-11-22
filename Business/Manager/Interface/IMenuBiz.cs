0649a1d1ac67a0f29b9b99eb3b18fff9a1d54f0c
EmreKabali <50817604+EmreKabali@users.noreply.github.com>
2021-11-22 07:36:30 +0300

    Menu business added

---
+++ Business/Manager/Interface/IMenuBiz.cs
@@ -0,0 +1,33 @@
+﻿using Entity.DTO.RequestDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Manager.Interface
+{
+   public interface IMenuBiz
+    {
+
+        MenuResponse GetById(int id);
+
+        MenuResponse GetByFilter(MenuRequest request);
+
+        MenuResponse GetList();
+
+
+        MenuResponse Add(MenuRequest request);
+
+        Task<MenuResponse> AddAsync(MenuRequest request);
+
+        MenuResponse Update(MenuRequest request);
+
+        Task<MenuResponse> UpdateAsync(MenuRequest request);
+
+
+        void Delete(MenuRequest request);
+
+        Task DeleteAsync(MenuRequest request);
+    }
+}
---
+++ Business/Manager/MenuBiz.cs
@@ -0,0 +1,58 @@
+﻿using Business.Manager.Interface;
+using Entity.DTO.RequestDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Manager
+{
+    public class MenuBiz : IMenuBiz
+    {
+        public MenuResponse Add(MenuRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MenuResponse> AddAsync(MenuRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(MenuRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(MenuRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MenuResponse GetByFilter(MenuRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MenuResponse GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MenuResponse GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public MenuResponse Update(MenuRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MenuResponse> UpdateAsync(MenuRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
