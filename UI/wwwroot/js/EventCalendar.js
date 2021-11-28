b2cd347564a2ea34a2c47f417dbc7705b493496b
EmreKabali <50817604+EmreKabali@users.noreply.github.com>
2021-11-28 15:12:04 +0300

    added fullcalendar plugin

---
+++ UI/wwwroot/js/EventCalendar.js
@@ -0,0 +1,35 @@
+﻿
+
+function OpenCalendar() {
+
+    document.addEventListener('DOMContentLoaded', function () {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth',
+            headerToolbar: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            },
+            selectable: true,
+            select: function OpenModal(selectInfo) {
+               
+
+                var startdate = selectInfo.startStr;
+                var endDate = selectInfo.endStr;
+                $("#exampleModal").modal('show');
+                
+                
+            },
+
+
+
+        });
+        calendar.render();
+    });
+  
+};
+
+
+
+
